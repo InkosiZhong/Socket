1704fb6267e1a671b0ac0f9ca7ab983c78784815
InkosiZhong <inkosizhong>
2021-08-26 10:13:56 +0800

    reconstruct SocketClient

--- Client/SocketClient.cpp
+++ Client/SocketClient.cpp
@@ -9,36 +9,31 @@ void* SocketClient::_receive(void* args){
 	TransPack pack;
 	while(1){
 		memset(pack.msg, 0, strlen(pack.msg) + 1);
-#ifdef __linux__
+#if defined(__APPLE__) || defined(__linux__)
 		usleep(1e4); // us
-#endif
-#ifdef WIN32
+#elif defined(_WIN32)
 		Sleep(10); // ms
 #endif
 		int ret = recv(targs.connfd, (char*)&pack, sizeof(pack), 0);
         if (ret > 0){
-			printf("receive form %d: %s ｜ %s\n", pack.sender, pack.msg, asctime(&pack.t));
-			if (!targs.connected && pack.sender == -1){
-				targs.connected = true;
-			}
-			else {
-				recv_buffer.push_back(pack);
-			}
+			printf("### receive form %d: %s ｜ %s\n", pack.sender, pack.msg, asctime(&pack.t));
+			recv_buffer.push_back(pack);
 		}
 		else if (ret < 0){
 			if (m_log)printf("### Failed to receive message(%d)\n", ret);
+			targs.connected = false;
 			break;
 		}
 		else {
 			if (m_log)printf("### Connection break.\n");
+			targs.connected = false;
 			break;
 		}
 	}
 	targs.connected = false;
-#if __linux__
+#if defined(__APPLE__) || defined(__linux__)
 	pthread_exit(NULL);
-#endif
-#ifdef WIN32
+#elif defined(_WIN32)
 	return NULL;
 #endif
 }
@@ -53,18 +48,24 @@ bool SocketClient::send(int recver, const char* msg){
 	pack.t = *localtime(&timer);
 	memcpy(pack.msg, msg, strlen(msg));
 	send_buffer.push_back(pack);
-	return true;
+	return isConnected();
 }
 
 bool SocketClient::send(TransPack& pack){
 	send_buffer.push_back(pack);
-	return true;
+	return isConnected();
 }
 
-int SocketClient::get(TransPack& pack){
+int SocketClient::get(TransPack& pack, int order){
 	if (recv_buffer.empty())return -1;
-	pack = recv_buffer.front();
-	recv_buffer.pop_front();
+	if (order == FRONT){
+		pack = recv_buffer.front();
+		recv_buffer.pop_front();
+	}
+	else {
+		pack = recv_buffer.back();
+		recv_buffer.pop_back();
+	}
 	return recv_buffer.size();
 }
 
@@ -80,37 +81,10 @@ void* SocketClient::_send(void* args){
 	Args& targs = *(Args*)args;
 	TransPack pack;
 	pack.sender = targs.account;
-	/* transmit account */
-	while(!targs.connected){
-		if (m_log)printf("### waiting for response...\n");
-		memset(pack.msg, 0, strlen(pack.msg) + 1);
-		pack.recver = -1;
-#if __linux__
-		usleep(1e5);
-#endif
-#ifdef WIN32
-		Sleep(100);
-#endif
-		time_t timer;
-		time(&timer);
-		pack.t = *localtime(&timer);
-		printf("send form %d to %d: %s | %s\n", pack.sender, pack.recver, pack.msg, asctime(&pack.t));
-		int ret = ::send(targs.connfd, (char*)&pack, sizeof(pack), 0);
-		if (ret < 0){
-			if (m_log)printf("### Failed to send account(%d)\n", ret);
-			break;
-		}
-        else if (ret == 0){
-			if (m_log)printf("### Connection break.\n");
-			break;
-		}
-	}
-	if (m_log)printf("### Connection established with server\n");
 	while(targs.connected){
-#if __linux__
+#if defined(__APPLE__) || defined(__linux__)
 		usleep(1e4);
-#endif
-#ifdef WIN32
+#elif defined(_WIN32)
 		Sleep(10);
 #endif
 		while (!send_buffer.empty()){
@@ -129,23 +103,24 @@ void* SocketClient::_send(void* args){
 			}
 		}
 	}
-#ifdef __linux__
+#if defined(__APPLE__) || defined(__linux__)
 	pthread_exit(NULL);
-#endif
-#ifdef WIN32
+#elif defined(_WIN32)
 	return NULL;
 #endif
 }
 
-SocketClient::SocketClient(bool log) { m_log = log; }
+SocketClient::SocketClient(bool log) { 
+	m_log = log; 
+	args.connected = false;
+}
 
 SocketClient::~SocketClient(){
-#ifdef __linux__
+#if defined(__APPLE__) || defined(__linux__)
 	pthread_cancel(recv_thread);
 	pthread_cancel(send_thread);
 	close(args.connfd);
-#endif
-#ifdef WIN32
+#elif defined(_WIN32)
 	closesocket(args.connfd);
 	WSACleanup();
 #endif
@@ -157,9 +132,10 @@ bool SocketClient::init(int account){
 }
 
 bool SocketClient::connect(const char* ip, const int port){
-#ifdef WIN32
+	int ret = 0;
+#if defined(_WIN32)
 	WSADATA wsaData;
-	int ret = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	ret = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (ret != 0) {
 		printf("Failed to load Winsock\n");
 		return false;
@@ -174,25 +150,28 @@ bool SocketClient::connect(const char* ip, const int port){
     serv_addr.sin_family = AF_INET;  //使用IPv4地址
     serv_addr.sin_addr.s_addr = inet_addr(ip);  //具体的IP地址
     serv_addr.sin_port = htons(port);  //端口
-    ::connect(args.connfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-	
-	args.connected = false;
+    ret = ::connect(args.connfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	if (ret != 0){
+		printf("### Failed to connect: error_code = %d\n", ret);
+		return false;
+	}
 
-#ifdef __linux__
-	int ret = pthread_create(&recv_thread, NULL, receive, (void*)&(args));
+	args.connected = true;
+
+#if defined(__APPLE__) || defined(__linux__)
+	ret = pthread_create(&recv_thread, NULL, _receive, (void*)&(args));
 	if (ret != 0) {
 		if (m_log)printf("### pthread_create for recv_thread error: error_code = %d\n", ret);
 		return false;
 	}
     pthread_detach(recv_thread);
-	ret = pthread_create(&send_thread, NULL, send, (void*)&(args));
+	ret = pthread_create(&send_thread, NULL, _send, (void*)&(args));
 	if (ret != 0) {
 		if (m_log)printf("### pthread_create for send_thread error: error_code = %d\n", ret);
 		return false;
 	}
     pthread_detach(send_thread);
-#endif
-#ifdef WIN32
+#elif defined(_WIN32)
 	recv_thread = std::thread(_receive, &args);
 	send_thread = std::thread(_send, &args);
 	recv_thread.detach();
--- Client/SocketClient.h
+++ Client/SocketClient.h
@@ -1,20 +1,19 @@
 #ifndef SOCKETCLIENT_H_
 #define SOCKETCLIENT_H_
-#define WIN32
 
 #define MAX_MSG_LEN 1024
+enum Order{FRONT, BACK};
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#ifdef __linux__
+#if defined(__APPLE__) || defined(__linux__)
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
-#endif
-#ifdef WIN32
+#elif defined(_WIN32)
 #include <winsock2.h>
 #include <windows.h>
 #include <thread> // c++11
@@ -37,14 +36,63 @@ struct Args{
 
 class SocketClient{
 public:
+	/** SocketClient
+	 * @brief 构造SocketClient对象
+	 * @param log: bool(true) 是否显示调试记录
+	 * @return SocketClient对象
+	*/
 	SocketClient(bool log = true);
 	~SocketClient();
+
+	/** init - debug
+	 * @brief 初始化账户，无数据库debug使用
+	 * @param account: int 非负账户
+	 * @return bool 是否初始化成功
+	*/
 	bool init(int account);
+
+	/** connect
+	 * @brief 建立Socket连接
+	 * @param ip: const char*("127.0.0.1") 服务器ip
+	 * @param port: const int(1234) 服务器端口
+	 * @return bool 是否连接
+	*/
 	bool connect(const char* ip = "127.0.0.1", const int port = 1234);
+
+	/** send - 不推荐
+	 * @brief 发送chat record，自动生成时间戳
+	 * @param recver: int 收信账户
+	 * @param msg: const char* 消息
+	 * @return bool 是否成功发送
+	*/
 	bool send(int recver, const char* msg);
+
+	/** send
+	 * @brief 发送Transmition Package
+	 * @param pack: TransPack& Transmition Package
+	 * @return bool 是否成功发送
+	*/
 	bool send(TransPack& pack);
-	int get(TransPack& pack);
+
+	/** get
+	 * @brief 获取单个待读Transmition Package
+	 * @param pack: TransPack& Transmition Package
+	 * @param order: int(FRONT), 获取顺序(FRONT最早, BACK最新)
+	 * @return int 成功: buffer内仍待读的包数, 失败: -1
+	*/
+	int get(TransPack& pack, int order = FRONT);
+
+	/** get
+	 * @brief 获取全部待读Transmition Package
+	 * @param pack: std::list<TransPack>& Transmition Package队列
+	 * @return int 成功: 此次获取到的包数, 失败: -1
+	*/
 	int get(std::list<TransPack>& list);
+
+	/** isConnected
+	 * @brief 获取连接状态
+	 * @return bool 连接状态
+	*/
 	bool isConnected();
 
 private:
@@ -52,10 +100,9 @@ private:
 	static void* _send(void* args);
 
 private:
-#ifdef __linux__
+#if defined(__APPLE__) || defined(__linux__)
 	pthread_t recv_thread, send_thread;
-#endif
-#ifdef WIN32
+#elif defined(_WIN32)
 	std::thread recv_thread, send_thread;
 #endif
 	Args args;
